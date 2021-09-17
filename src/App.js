f82af35722c67d5bd8cb0c5366bed76ad0ad9ef0
dishant-digiqt <dishant.rathi@digiqt.com>
2021-09-17 15:30:46 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Suspense } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Layout from './components/Layout'
+import Layout from "./components/pokemon";
 
 const App = () => {
-	return (
-		<Router>
-			<Suspense fallback={<p>loading...</p>}>
-				<Switch>
-					<Route exact path='/' name='Layout' render={() => <Layout />} />
-				</Switch>
-			</Suspense>
-		</Router>
-	)
-}
+  return (
+    <Router>
+      <Suspense fallback={<p>loading...</p>}>
+        <Switch>
+          <Route exact path="/" name="Layout" render={() => <Layout />} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
 
-export default App
+export default App;
